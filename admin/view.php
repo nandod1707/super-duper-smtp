256cc4d9b1eeb1158066df4a796ee1787fc2ee11
Nando Delgado <nando.emmanuel@gmail.com>
2020-04-05 00:49:10 -0300

    Options page wip

--- admin/view.php
+++ admin/view.php
@@ -3,11 +3,14 @@
 require_once plugin_dir_path( __FILE__ ) . 'class-super-duper-smtp-admin.php';
 
 ?>
-<h1>Contact Forms</h1>
+<h1>Super Duper SMTP</h1>
 
-<form action="">
+<form action="#">
     <p>
-        <input id="use-smtp" type="checkbox">
+        <input id="use-smtp" type="checkbox" checked>
         <label for="use-smtp">Send emails using Super Duper SMTP</label>
+    </p>
+    <p>
+        <input type="submit" value="Save">
     </p>
 </form>
